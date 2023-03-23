c0d8e902fd53d2b7719f780c8da150923575b5d5
Carlos Ormeño <ormenovargasc@gmail.com>
2023-03-23 21:23:02 +0000

    add ajax and html structure to snippets

--- gift_recommender/snippets/prompt_and_add_products.js
+++ gift_recommender/snippets/prompt_and_add_products.js
@@ -1,37 +1,3 @@
-function getProductHtml(product) {
-    return `<li class="ast-article-single desktop-align-left tablet-align-left mobile-align-left product type-product post-316 status-publish first instock product_cat-paste-masks has-post-thumbnail shipping-taxable purchasable product-type-simple">
-    <div class="astra-shop-thumbnail-wrap">
-        <a href="${product.detail_page}" class="woocommerce-LoopProduct-link woocommerce-loop-product__link">
-            <img width="300" height="300" src="${product.image_url}"
-                class="attachment-woocommerce_thumbnail size-woocommerce_thumbnail" alt="" decoding="async">
-        </a>
-    </div>
-    <div class="astra-shop-summary-wrap"> 
-        <a href="${product.detail_page}" class="ast-loop-product__link">
-            <h2 class="woocommerce-loop-product__title">${product.name}</h2>
-        </a>
-        <span class="price">
-            <span class="woocommerce-Price-amount amount">
-                <bdi>${product.display_amount}</bdi>
-            </span>
-        </span>
-    </div>
-</li>`
-}
-
-function getListProductsHtml(products){
-    var $productListWrapperHtml = jQuery('<div class="woocommerce columns-4"></div>')
-    var $productListHtml = jQuery('<ul class="products columns-4"></ul>')
-    for(var i=0; i<products.length; i++){
-        $productListHtml.append(getProductHtml(products[i]));
-    }
-    return $productListWrapperHtml.append($productListHtml);
-}
-
-function getCategoryHtml(category){
-    return `<div class="uagb-ifb-content"><div class="uagb-ifb-title-wrap"><h2 class="uagb-ifb-title">${category.description}</h2></div></div>`
-}
-
 const LIMIT_CHARACTERS = 40;
 
 const ERROR_AT_LEAST_CHARACTERS = "To proceed, please ensure that your text contains at least 40 characters.";
@@ -47,89 +13,80 @@ var $loaderWrapper = jQuery(".loader-wrapper");
 var $promptWrapper = jQuery(".prompt-wrapper");
 var $promptField = jQuery("#prompt");
 var $promptError = jQuery("#prompt-error");
-var products = [
-    {
-    'name': "example name",
-    'asin': "example asin",
-    'detail_page': "http://159.89.190.30/product/rose-essential-oil/",
-    'image_url': "http://159.89.190.30/wp-content/uploads/2021/03/rose-essential-oil-300x300.jpg",
-    'display_amount': "50.00"
-},
-    {
-    'name': "example name",
-    'asin': "example asin",
-    'detail_page': "http://159.89.190.30/product/rose-essential-oil/",
-    'image_url': "http://159.89.190.30/wp-content/uploads/2021/03/rose-essential-oil-300x300.jpg",
-    'display_amount': "50.00"
-},
-    {
-    'name': "example name",
-    'asin': "example asin",
-    'detail_page': "http://159.89.190.30/product/rose-essential-oil/",
-    'image_url': "http://159.89.190.30/wp-content/uploads/2021/03/rose-essential-oil-300x300.jpg",
-    'display_amount': "50.00"
-},
-    {
-    'name': "example name",
-    'asin': "example asin",
-    'detail_page': "http://159.89.190.30/product/rose-essential-oil/",
-    'image_url': "http://159.89.190.30/wp-content/uploads/2021/03/rose-essential-oil-300x300.jpg",
-    'display_amount': "50.00"
-},
-    {
-    'name': "example name",
-    'asin': "example asin",
-    'detail_page': "http://159.89.190.30/product/rose-essential-oil/",
-    'image_url': "http://159.89.190.30/wp-content/uploads/2021/03/rose-essential-oil-300x300.jpg",
-    'display_amount': "50.00"
-},
-    {
-    'name': "example name",
-    'asin': "example asin",
-    'detail_page': "http://159.89.190.30/product/rose-essential-oil/",
-    'image_url': "http://159.89.190.30/wp-content/uploads/2021/03/rose-essential-oil-300x300.jpg",
-    'display_amount': "50.00"
-},
-    {
-    'name': "example name",
-    'asin': "example asin",
-    'detail_page': "http://159.89.190.30/product/rose-essential-oil/",
-    'image_url': "http://159.89.190.30/wp-content/uploads/2021/03/rose-essential-oil-300x300.jpg",
-    'display_amount': "50.00"
+
+function getProductHtml(product) {
+    return `<li class="ast-article-single desktop-align-left tablet-align-left mobile-align-left product type-product post-316 status-publish first instock product_cat-paste-masks has-post-thumbnail shipping-taxable purchasable product-type-simple">
+    <div class="astra-shop-thumbnail-wrap">
+        <a href="${product.detail_page}" class="woocommerce-LoopProduct-link woocommerce-loop-product__link" target="_blank">
+            <img width="300" height="300" src="${product.image_url}"
+                class="attachment-woocommerce_thumbnail size-woocommerce_thumbnail" alt="" decoding="async">
+        </a>
+    </div>
+    <div class="astra-shop-summary-wrap"> 
+        <a href="${product.detail_page}" class="ast-loop-product__link" target="_blank">
+            <h2 class="woocommerce-loop-product__title">${product.name}</h2>
+        </a>
+        <span class="price">
+            <span class="woocommerce-Price-amount amount">
+                <bdi>${product.display_amount}</bdi>
+            </span>
+        </span>
+    </div>
+</li>`
 }
-]
-var category = {
-    'description': "Example of Category"
+
+function getListProductsHtml(products){
+    var $productListWrapperHtml = jQuery('<div class="woocommerce columns-5"></div>')
+    var $productListHtml = jQuery('<ul class="products columns-5"></ul>')
+    for(var i=0; i<products.length; i++){
+        $productListHtml.append(getProductHtml(products[i]));
+    }
+    return $productListWrapperHtml.append($productListHtml);
+}
+
+function getCategoryHtml(category){
+    return `<div class="uagb-ifb-content"><div class="uagb-ifb-title-wrap"><h2 class="uagb-ifb-title">${category.description}</h2></div></div>`
+}
+
+function getLoaderHtml(){
+    return '<div class="loader-wrapper d-block"><span class="loader"></span></div>'
+}
+
+function hide($el){
+    return $el.removeClass("d-block").addClass("d-none");
+}
+function show($el){
+    return $el.removeClass("d-none").addClass("d-block");
 }
 
 function showMainLoader(){
-    $loaderWrapper.removeClass("d-none").addClass("d-block");
+    return show($loaderWrapper);
 }
 function hideMainLoader(){
-    $loaderWrapper.removeClass("d-block").addClass("d-none");
+    return hide($loaderWrapper);
 }
 
 function hidePromptField(){
-    $promptWrapper.removeClass("d-block").addClass("d-none");
+    return hide($promptWrapper);
 }
 
 function showPromptField(){
-    $promptWrapper.removeClass("d-none").addClass("d-block");
+    return show($promptWrapper);
 }
 
 function showProductsSection(){
-    $productsSection.removeClass("d-none").addClass("d-block");
+    return show($productsSection);
 }
 
 function hideProductsSection(){
-    $productsSection.removeClass("d-block").addClass("d-none");
+    return hide($productsSection);
 }
 
 function clearProductsSection(){
-    $productsSection.html("");
+    $productsSection.empty();
 }
 
-function clearPromptfield(){
+function clearPromptField(){
     $promptField.val("");
 }
 
@@ -138,11 +95,19 @@ function getPromptFieldValue(){
 }
 
 function showErrorPrompt(errorName){
-    $promptError.removeClass("d-none").addClass("d-block").html(errorName);
+    return show($promptError).html(errorName);
 }
 
 function hideErrorPrompt(){
-    $promptError.removeClass("d-block").addClass("d-none").html("");
+    return hide($promptError).html("");
+}
+
+function showBtnGenerateIdeas(){
+    return show($generateBtn)
+}
+
+function hideBtnGenerateIdeas(){
+    return hide($generateBtn)
 }
 
 function getRemoteCategories(data){
@@ -150,13 +115,47 @@ function getRemoteCategories(data){
     return jQuery.ajax({
         type: "POST",
         url: apiBasePath+promptPath,
-        data: data,
+        data: JSON.stringify(data),
         contentType:"application/json; charset=utf-8",
         dataType: "json"
-    });
+    }).retry({times:3});
 }
 
+function getRemoteProducts(data){
+    // return jQuery.post(apiBasePath+promptPath, data, "json");
+    return jQuery.ajax({
+        type: "POST",
+        url: apiBasePath+productsPath,
+        data: JSON.stringify(data),
+        contentType:"application/json; charset=utf-8",
+        dataType: "json"
+    }).retry({times:3});
+}
+
+
+function renderCategoryAndProducts(category){
+    var $container = jQuery("<div></div>").appendTo($productsSection);
+    $container.append(getCategoryHtml(category));
+    var $loader = jQuery(getLoaderHtml()).appendTo($container);
+    getRemoteProducts({category: category.description})
+    .done(function(data){
+        console.log('Success: ', data );
+         var $products = getListProductsHtml(data);
+        $container.append($products);
+    })
+    .fail(function(){
+        $container.remove();
+    })
+    .always(function(){
+        $loader.remove();
+    })
+}
+
+var loading = false;
 $generateBtn.on("click", function(e){
+    if(loading)return;
+    loading = true;
+    hideBtnGenerateIdeas();
     hideErrorPrompt();
     var prompt = getPromptFieldValue();
     if(prompt.length < LIMIT_CHARACTERS){
@@ -167,27 +166,23 @@ $generateBtn.on("click", function(e){
     showMainLoader();
     clearProductsSection();
     hideProductsSection();
-
+    // hidePromptField();
     getRemoteCategories({statement: prompt})
         .done(function(data){
-            console.log('Success: ', { data } );
+            console.log('Success: ', data );
+            showProductsSection();
+            var categories = data;
+            categories.map(category => renderCategoryAndProducts(category));
         })
         .fail(function(){
             showErrorPrompt(ERROR_GENERIC);
+            // showPromptField();
+        })
+        .always(function(){
             hideMainLoader();
-            showPromptField();
+            showBtnGenerateIdeas();
+            loading=false;
         })
 
     e.preventDefault();
 })
-
-
-
-
-
-// if ($productsSection.size() > 0) {
-//     $productsSection.append(getCategoryHtml(category));
-//     $productsSection.append(getListProductsHtml(products));
-//     $productsSection.append(getCategoryHtml(category));
-//     $productsSection.append(getListProductsHtml(products));
-// }
